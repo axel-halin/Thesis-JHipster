a2003bb0988e01b11c763fd9d477fb7009deabc2
Nuttinck Alexandre <nuttinck_alexandre@hotmail.com>
2016-10-03 09:34:11 +0200

    Add time limit - build (oracle)

--- FML-brute/src/Oracle.java
+++ FML-brute/src/Oracle.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,66 +30,77 @@ public class Oracle {
 	private String projectDirectory = System.getProperty("user.dir");
 
 	/**
-	 * Creates the script for windows. //TODO !!
-	 * 
-	 * @param conf JHipster configuration to be generated.
+	 * @param nameScriptBat Name of the Script .bat
+	 * @param nameScriptSh Name of the Sript .sh
+	 * @param jDirectory Name of the folder
 	 */
 	private void writeScriptBat(String nameScriptBat,String nameScriptSh,String jDirectory)
 	{
-		String buildScript = "cd "+getjDirectory(jDirectory)+"\n"; 
-		//buildScript += "create database if not exists \"jhipster-fou\";\n";
-		//buildScript += "\"C:\Program Files\Git\bin\sh.exe\" --login ./" + nameScript;
-		buildScript += "\"C:/Program Files/Git/bin/sh.exe\" --login ./" + nameScriptSh;
-		Files.writeStringIntoFile(getjDirectory(jDirectory) + nameScriptBat, buildScript);
+		if (nameScriptBat != "bashgitlaunchDatabases.bat")
+		{
+			String buildScript = "cd "+getjDirectory(jDirectory)+"\n"; 
+
+			buildScript += "\"C:/Program Files/Git/bin/sh.exe\" --login ./" + nameScriptSh;
+
+			Files.writeStringIntoFile(getjDirectory(jDirectory) + nameScriptBat, buildScript);
+
+		}
+		else
+		{
+			String buildScript =  "\"C:/Program Files/Git/bin/sh.exe\" --login ./" + nameScriptSh;
+			Files.writeStringIntoFile("bashgitlaunchDatabases.bat", buildScript);
+		}
+
 	}
-	
+
 	/**
 	 * Creates the script to launch all databases 
 	 * 
 	 */
 	private void writeScriptDatabases(){
-		
+
 		String buildScript = "#!/bin/bash\n\n";
-		
-			//CASSANDRA
-			buildScript += "echo 'Cassandra!'\n";
-			buildScript += "services cassandra start\n";
-			buildScript += "cqlsh -f src/main/resources/config/cql/create-keyspace.cql\n";
-			buildScript += "cqlsh -f src/main/resources/config/cql/changelog/00000000000000_create-tables.cql\n";
-			buildScript += "cqlsh -f src/main/resources/config/cql/changelog/00000000000001_insert_default_users.cql\n";
-		
-			//MONGODB
-			buildScript += "echo 'MongoDB!'\n";
-			buildScript += "services mongodb start\n";
 
-			// ORACLE TODO
-		
-			//POSTGRE
-			
-			buildScript += "echo 'Postgres!'\n";
-			buildScript += "services pgservice start\n";
-			buildScript += "psql -U postgres <<EOF\n";
-			buildScript += "create role \"jhipster-fou\" login;\n";
-			buildScript += "create database \"jhipster-fou\";\n";
-			buildScript += "\\q\n";
-			buildScript += "EOF\n";
-		
-			//MARIADB + MYSQL
+		//CASSANDRA
+		buildScript += "echo 'Cassandra!'\n";
+		buildScript += "services cassandra start\n";
+		buildScript += "cqlsh -f src/main/resources/config/cql/create-keyspace.cql\n";
+		buildScript += "cqlsh -f src/main/resources/config/cql/changelog/00000000000000_create-tables.cql\n";
+		buildScript += "cqlsh -f src/main/resources/config/cql/changelog/00000000000001_insert_default_users.cql\n";
 
-			buildScript += "echo 'MariaDB-SQL!'\n";
-			buildScript += "services mysql start\n";
-			buildScript += "mysql -u root <<EOF\n";
-			buildScript += "SET SESSION sql_mode = 'ANSI';\n";
-			buildScript += "create database if not exists \"jhipster-fou\";\n";
-			buildScript += "\\q\n";
-			buildScript += "EOF\n";
-		
+		//MONGODB
+		buildScript += "echo 'MongoDB!'\n";
+		buildScript += "services mongodb start\n";
+
+		// ORACLE TODO
+
+		//POSTGRE
+
+		buildScript += "echo 'Postgres!'\n";
+		buildScript += "services pgservice start\n";
+		buildScript += "psql -U postgres <<EOF\n";
+		buildScript += "create role \"jhipster-fou\" login;\n";
+		buildScript += "create database \"jhipster-fou\";\n";
+		buildScript += "\\q\n";
+		buildScript += "EOF\n";
+
+		//MARIADB + MYSQL
+
+		buildScript += "echo 'MariaDB-SQL!'\n";
+		buildScript += "services mysql start\n";
+		buildScript += "mysql -u root <<EOF\n";
+		buildScript += "SET SESSION sql_mode = 'ANSI';\n";
+		buildScript += "create database if not exists \"jhipster-fou\";\n";
+		buildScript += "\\q\n";
+		buildScript += "EOF\n";
+
 		Files.writeStringIntoFile("launchDatabases.sh", buildScript);
 	}
-	
+
 	/**
 	 * Launch Databases.
 	 * 
+	 * @param system boolean type of the system (linux then true, else false)
 	 * @throws InterruptedException 
 	 * @throws IOException 
 	 */
@@ -125,11 +137,12 @@ public class Oracle {
 
 
 	}
-	
+
 	/**
 	 * Generate the App from the yo-rc.json.
 	 * 
 	 * @param jDirectory Name of the folder
+	 * @param system boolean type of the system (linux then true, else false)
 	 * @throws InterruptedException 
 	 * @throws IOException 
 	 */
@@ -180,7 +193,7 @@ public class Oracle {
 		String text = "";
 
 		//extract log
-		text = Files.readFileIntoString(getjDirectory(jDirectory) + "jhipsteryo.log");
+		text = Files.readFileIntoString(getjDirectory(jDirectory) + "generate.log");
 
 		//CHECK IF Server app generated successfully.
 		//OR Client app generated successfully.
@@ -200,6 +213,7 @@ public class Oracle {
 	 * Build the App which is generated successfully
 	 * 
 	 * @param jDirectory Name of the folder
+	 * @param system boolean type of the system (linux then true, else false)
 	 * @throws InterruptedException 
 	 */
 	private void buildApp(String jDirectory, boolean system) throws InterruptedException{
@@ -212,7 +226,7 @@ public class Oracle {
 				ProcessBuilder pb = new ProcessBuilder(getjDirectory(jDirectory) + "bashgitbuild.bat");
 				pb.inheritIO();
 				Process process = pb.start();
-				process.waitFor();
+				process.waitFor(200, TimeUnit.SECONDS);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -248,7 +262,7 @@ public class Oracle {
 		String text = "";
 
 		//extract log
-		text = Files.readFileIntoString(getjDirectory(jDirectory) + "jhipsterbuild.log");
+		text = Files.readFileIntoString(getjDirectory(jDirectory) + "build.log");
 
 
 		//CHECK IF APPLICATION FAILED TO START THEN false
@@ -315,7 +329,7 @@ public class Oracle {
 	private String getjDirectory(String jDirectory) {
 		return JHIPSTERS_DIRECTORY + "/" + jDirectory + "/";
 	}
-	
+
 	/**
 	 * Return the value of the system 
 	 * 
@@ -338,7 +352,7 @@ public class Oracle {
 			}
 
 			Boolean system = Boolean.parseBoolean(prop.getProperty("system"));
-			
+
 			return system;
 
 		} catch (Exception e) {
@@ -354,7 +368,7 @@ public class Oracle {
 	 */
 	@Test
 	public void generateJHipsterVariants() throws Exception{
-		
+
 		//false = windows
 		//true = linux
 		Boolean system = getValueOfSystem();
@@ -366,15 +380,15 @@ public class Oracle {
 		for (Integer i =1;i<=weightFolder;i++){
 
 			String jDirectory = "jhipster"+i;
-			
+
 			if (!system)
 				//write .bat Scripts for windows
 			{
-			writeScriptBat("bashgitgenerate.bat","generate.sh",jDirectory);
-			writeScriptBat("bashgitbuild.bat","build.sh",jDirectory);
-			writeScriptBat("bashgittest.bat","test.sh",jDirectory);
+				writeScriptBat("bashgitgenerate.bat","generate.sh",jDirectory);
+				writeScriptBat("bashgitbuild.bat","build.sh",jDirectory);
+				writeScriptBat("bashgittest.bat","test.sh",jDirectory);
 			}
-			
+
 			_log.info("Extracting files from jhipster "+i+"...");
 			_log.info(getjDirectory(jDirectory));
 
@@ -389,7 +403,7 @@ public class Oracle {
 	/**
 	 * Check all generation of JHipster 3.6.1. and build the app if the generation is OK.
 	 */
-	//@Test
+	@Test
 	public void checkAndBuildJHipsterVariants() throws Exception{
 
 		// weight of Folder Jhipsters
@@ -399,7 +413,13 @@ public class Oracle {
 		// false = windows
 		//true = linux
 		Boolean system = getValueOfSystem();
-		
+
+		if (!system)
+			//write .bat Scripts for windows
+		{
+			writeScriptBat("bashgitlaunchDatabases.bat","launchDatabases.sh","");
+		}
+
 		// LAUNCH DATABASES AT THE BEGINING
 		writeScriptDatabases();
 		_log.info("Launch Databases...");
