07f38e244c4c3271e8d18232ca64cfa7f91d1f0c
Axel <root@Axel.home>
2016-10-18 15:56:04 +0200

    Conflict Solving

--- FML-brute/src/oracle/Oracle.java
+++ FML-brute/src/oracle/Oracle.java
@@ -176,8 +176,6 @@ public class Oracle {
 		return JHIPSTERS_DIRECTORY + "/" + jDirectory + "/";
 	}
 
-
-
 	/**
 	 * Launch initialization scripts:\n
 	 * 		- Start Uaa Server (in case of Uaa authentication)
